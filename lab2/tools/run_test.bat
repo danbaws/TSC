3a329135df621fa08bd60c5fbb2199a6695cff49
Dan <daniel.milea@student.unitbv.ro>
2024-03-20 09:20:40 +0200

    Laborator5

--- lab2/tools/run_test.bat
+++ lab2/tools/run_test.bat
@@ -4,4 +4,5 @@ call clean.bat
 call build.bat
 ::========================================================================================
 cd ../sim
-vsim -gui -do run.do
+::vsim -gui -do run.do
+vsim -c -do run.do
